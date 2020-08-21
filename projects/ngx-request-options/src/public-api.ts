9a8e84670300f19e5762bc20a97d451950857b3c
ezzabuzaid <Ezzabuzaid@hotmail.com>
2020-08-21 15:55:44 +0300

    Update public-api.ts
    fix(RequestOptionModule): export REQUEST_OPTIONS_DEFAULT injection token
    REQUEST_OPTIONS_DEFAULT used to get the default options

--- projects/ngx-request-options/src/public-api.ts
+++ projects/ngx-request-options/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/request-options.service';
 export * from './lib/http.service';
 export * from './lib/request-options.interceptor';
 export * from './lib/request-options.module';
+export {REQUEST_OPTIONS_DEFAULT} from './lib/types';
